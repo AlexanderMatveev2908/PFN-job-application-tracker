507c34e84658099d59d4bc0b49f1aab98f0f46fc
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-31 13:29:59 +0300

    main => fix logger

--- src/lib/logger.py
+++ src/lib/logger.py
@@ -12,13 +12,19 @@ def __cg(
         f"{(f'{ttl} 🔥'.center(len(ttl) + 4, ' ')).center(len(ttl) * 4, '—')}"
     )
 
-    caller_file = Path(inspect.stack()[1].filename).resolve()
-
     for x in arg:
         print(x)
 
     now = datetime.now()
-    time_parsed = f'⏰ => {now.strftime("%H:%M:%S")}'
+    time_parsed = f'⏰ => at {now.strftime("%H:%M:%S")}'
     print(time_parsed)
 
+    frame = ""
+    for fr in inspect.stack():
+        filename = fr.filename
+        if "src/" in filename and "lib/logger" not in filename:
+            frame = fr
+            break
+
+    caller_file = Path(frame.filename if frame else "").resolve()
     print(f"📌 => from {caller_file}")
