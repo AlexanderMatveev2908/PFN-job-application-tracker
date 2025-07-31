78d2d313571a035f101b814b0fd1ca05f4200cfc
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-31 13:38:51 +0300

    main => added func to write file

--- src/lib/system.py
+++ src/lib/system.py
@@ -9,4 +9,13 @@ script_dir = script_file.parent
 
 app_dir = Path.cwd()
 
-# print(app_dir)
+
+def write_f(relative: str, content: str) -> None:
+    joined = app_dir.joinpath(relative)
+    joined.parent.mkdir(parents=True, exist_ok=True)
+
+    if not joined.exists():
+        joined.touch()
+
+    with open(joined, "w") as f:
+        f.write(content)
