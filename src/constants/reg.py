025e55ef2a398238380694689b6eca3cd5cfd0e1
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-01 19:15:38 +0300

    main => added regex

--- src/constants/reg.py
+++ src/constants/reg.py
@@ -1,5 +1,10 @@
 import re
 
+import regex
+
+
+REG_TXT = regex.compile(r"^[\p{L}\d\s\-\'\",;!?]*$", flags=regex.UNICODE)
+
 REG_INT = re.compile(r"^\d+$")
 REG_FLOAT = re.compile(r"(^\d+(\.\d{1,2})?$)|(^\.\d{1,2}$)")
 REG_ID = re.compile(
