68aa22f21f8a95c67266feb25135bfd59373f629
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-02 15:15:53 +0300

    main => _

--- src/lib/s3.py
+++ src/lib/s3.py
@@ -1,27 +1,31 @@
 import datetime
 from io import BytesIO
+from pathlib import Path
 from mypy_boto3_s3 import S3Client
 
 from src.conf.s3 import gen_s3_session
 from ..conf.env import env_var
 
 
-async def upload_single(file: dict, base_path: str, s3: S3Client) -> dict:
+async def upload_single(file: dict, s3: S3Client) -> dict:
+    base_path = f"{env_var.app_name}"
     content_type = file["content_type"]
-    extra_args = {
-        "ContentType": content_type,
-        "Metadata": {
-            "created_at": datetime.datetime.now().isoformat(),
-        },
-    }
-    public_id = (
-        f"{base_path}/{'videos' if 'video/' in content_type else 'images'}"
-        f"/{file['filename']}"
+
+    public_id = str(
+        Path(base_path)
+        / ("videos" if content_type.startswith("video/") else "images")
+        / file["filename"]
     )
+
     args_aws = {
         "Bucket": env_var.aws_bucket_name,
         "Key": public_id,
-        "ExtraArgs": extra_args,
+        "ExtraArgs": {
+            "ContentType": content_type,
+            "Metadata": {
+                "created_at": datetime.datetime.now().isoformat(),
+            },
+        },
     }
 
     if "video/" in content_type:
@@ -30,7 +34,6 @@ async def upload_single(file: dict, base_path: str, s3: S3Client) -> dict:
         )
     else:
         buff_stream = BytesIO(file["buffer"])
-
         await s3.upload_fileobj(  # type: ignore[func-returns-value]
             buff_stream, **args_aws
         )
@@ -47,13 +50,10 @@ async def upload_w3(v: dict | list[dict]) -> dict | list[dict]:
 
     async with gen_s3_session() as s3:
 
-        base_path = f"{env_var.app_name}"
-        args = {"base_path": base_path, "s3": s3}
-
         if isinstance(v, list):
-            return [await upload_single(f, **args) for f in v]
+            return [await upload_single(f, s3) for f in v]
         else:
-            return await upload_single(v, **args)
+            return await upload_single(v, s3)
 
 
 async def gen_presigned_url(public_id: str) -> str:
