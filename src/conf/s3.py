13ee1a675e4d30cd1615170d5b81812e04b06f7d
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-02 13:56:17 +0300

    main => test upload of video also

--- src/conf/s3.py
+++ src/conf/s3.py
@@ -1,65 +1,16 @@
-import datetime
-from io import BytesIO
+from contextlib import asynccontextmanager
 import aioboto3
-from mypy_boto3_s3 import S3Client
 from .env import env_var
 
 
-async def upload_single(file: dict, base_path: str, s3: S3Client) -> dict:
-    content_type = file["content_type"]
-
-    extra_args = {
-        "ContentType": content_type,
-        "Metadata": {
-            "uploaded_by": "ninja",
-            "app": env_var.app_name,
-            "created_at": datetime.datetime.now().isoformat(),
-        },
-    }
-
-    if "video/" in content_type:
-        public_id = f"{base_path}/videos/{file['filename']}"
-
-        await s3.upload_file(  # type: ignore[func-returns-value]
-            file["path"],
-            Bucket=env_var.aws_bucket_name,
-            Key=public_id,
-            ExtraArgs=extra_args,
-        )
-    else:
-        public_id = f"{base_path}/images/{file['filename']}"
-        buff_stream = BytesIO(file["buffer"])
-
-        await s3.upload_fileobj(  # type: ignore[func-returns-value]
-            buff_stream,
-            Bucket=env_var.aws_bucket_name,
-            Key=public_id,
-            ExtraArgs=extra_args,
-        )
-
-    public_url = (
-        f"https://{env_var.aws_bucket_name}.s3."
-        f"{env_var.aws_region}.amazonaws.com/{public_id}"
-    )
-
-    return {"public_id": public_id, "public_url": public_url}
-
-
-async def upload_w3(v: dict | list[dict]) -> dict | list[dict]:
+@asynccontextmanager
+async def gen_s3_session():
     session = aioboto3.Session()
 
-    async with session.client(  # type: ignore[attr-defined]
+    async with session.client(  # type: ignore[func-returns-value]
         "s3",
         aws_access_key_id=env_var.aws_access_key,
         aws_secret_access_key=env_var.aws_access_secret_key,
         region_name=env_var.aws_region,
     ) as s3:
-
-        base_path = f"{env_var.app_name}"
-
-        if isinstance(v, list):
-            return [
-                await upload_single(f, base_path=base_path, s3=s3) for f in v
-            ]
-        else:
-            return await upload_single(v, base_path=base_path, s3=s3)
+        yield s3
--- src/features/test/controllers/post.py
+++ src/features/test/controllers/post.py
@@ -1,9 +1,8 @@
 import asyncio
 from fastapi import Depends, Request
-from src.conf.s3 import upload_w3
 from src.decorators.res import ResAPI
 from src.features.test.middleware.check_user import User, check_user
-from src.lib.logger import clg
+from src.lib.s3 import upload_w3
 from src.lib.system import del_vid
 
 
@@ -15,12 +14,14 @@ async def post_test(_: Request, user: User = Depends(check_user)) -> ResAPI:
 async def post_form(req: Request) -> ResAPI:
 
     parsed_f = req.state.parsed_f
-    del_vid(parsed_f)
 
     images = parsed_f["images"]
 
-    result = await asyncio.gather(*(upload_w3(img) for img in images))
+    uploaded_images = await asyncio.gather(*(upload_w3(img) for img in images))
+    uploaded_video = await upload_w3(parsed_f["video"])
 
-    clg(result, ttl="aws images")
+    del_vid(parsed_f)
 
-    return ResAPI.ok_201(result=result)
+    return ResAPI.ok_201(
+        uploaded_images=uploaded_images, uploaded_video=uploaded_video
+    )
