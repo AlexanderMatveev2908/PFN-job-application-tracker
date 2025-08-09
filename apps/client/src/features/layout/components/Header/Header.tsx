09f1a457f3dc5d9bdb148de9ea0be54328e099ad
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-09 15:16:33 +0300

    sidebar__template => added animation sidebar

--- apps/client/src/features/layout/components/Header/Header.tsx
+++ apps/client/src/features/layout/components/Header/Header.tsx
@@ -7,11 +7,17 @@ import SvgUser from "@/common/components/SVGs/User";
 import { css } from "@emotion/react";
 import Link from "next/link";
 import type { FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getSideState, sideSlice } from "../Sidebar/slice";
+import SvgClose from "@/common/components/SVGs/Close";
 
 const Header: FC = () => {
+  const sideState = useSelector(getSideState);
+
+  const dispatch = useDispatch();
   return (
     <div
-      className="w-full sticky top-0 left-0 border-b-3 border-w_0 flex items-center justify-between px-3"
+      className="z__header w-full sticky top-0 left-0 border-b-3 border-w_0 flex items-center justify-between px-3"
       css={css`
         height: ${75}px;
       `}
@@ -25,8 +31,20 @@ const Header: FC = () => {
           <SvgUser className="svg__xl text-w_0" />
         </button>
 
-        <button>
-          <SvgBurger className="svg__xl text-w_0" />
+        <button
+          onClick={() => dispatch(sideSlice.actions.toggleSide())}
+          className="btn__app"
+          style={
+            {
+              "--scale__up": 1.3,
+            } as React.CSSProperties
+          }
+        >
+          {sideState.isOpen ? (
+            <SvgClose className="svg__xl text-red-600" />
+          ) : (
+            <SvgBurger className="svg__xl text-w_0" />
+          )}
         </button>
       </div>
     </div>
--- apps/client/src/features/layout/components/Sidebar/Sidebar.tsx
+++ apps/client/src/features/layout/components/Sidebar/Sidebar.tsx
@@ -6,20 +6,40 @@ import { css } from "@emotion/react";
 import { headerHight } from "@/core/constants/style";
 import { useSelector } from "react-redux";
 import { getSideState } from "./slice";
-import { __cg } from "@/core/lib/log";
+import BlackBg from "@/common/components/elements/BlackBg";
+import { motion } from "framer-motion";
 
 const Sidebar: FC = () => {
   const sideState = useSelector(getSideState);
 
-  __cg(sideState);
-
   return (
-    <div
-      className="fixed h-full border-l-3 border-w_0 right-0 w-[80vw] sm:w-[500px] md:w-[600px]"
-      css={css`
-        top: ${headerHight}px;
-      `}
-    ></div>
+    <>
+      <BlackBg
+        {...{
+          classIndexCSS: "z__bg_sidebar",
+          isDark: sideState.isOpen,
+        }}
+      />
+
+      <motion.div
+        className="z__sidebar fixed h-full border-l-3 border-w_0 right-0 w-[80vw] sm:w-[500px] md:w-[600px] bg-neutral-950"
+        css={css`
+          top: ${headerHight}px;
+        `}
+        initial={{
+          x: "100%",
+          opacity: 0,
+        }}
+        transition={{
+          x: { duration: 0.4, ease: "easeInOut" },
+          opacity: { duration: 0.3, ease: "linear" },
+        }}
+        animate={{
+          x: sideState.isOpen ? "0" : "100%",
+          opacity: sideState.isOpen ? 1 : 0,
+        }}
+      ></motion.div>
+    </>
   );
 };
 
