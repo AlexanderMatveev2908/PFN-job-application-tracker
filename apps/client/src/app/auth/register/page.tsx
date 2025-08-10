4775f044f233b4e9f77e321c216d7c3a28e60d71
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-10 13:46:22 +0300

    register_page => defined base shape input & textarea

--- apps/client/src/app/auth/register/page.tsx
+++ apps/client/src/app/auth/register/page.tsx
@@ -5,23 +5,34 @@ import {
   RegisterFormT,
   registerSchema,
 } from "@/features/auth/schemas/register";
-import type { FC } from "react";
+import { useEffect, type FC } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import FormFieldTxt from "@/common/components/forms/inputs/FormFieldTxt";
 import { registerField } from "@/features/auth/uiFactory/register";
+import { useFocus } from "@/core/hooks/etc/useFocus";
+import { __cg } from "@/core/lib/log";
 
 const Page: FC = () => {
   const formCtx = useForm<RegisterFormT>({
     mode: "onChange",
     resolver: zodResolver(registerSchema),
   });
+  const { control, setFocus, watch } = formCtx;
+
+  useFocus("first_name", { setFocus });
+
+  const vls = watch();
+  useEffect(() => {
+    __cg(vls);
+  }, [vls]);
 
   return (
     <WrapAuthForm>
       <FormFieldTxt
         {...{
           el: registerField,
+          control,
         }}
       />
     </WrapAuthForm>
--- apps/client/src/common/components/forms/inputs/FormFieldTxt.tsx
+++ apps/client/src/common/components/forms/inputs/FormFieldTxt.tsx
@@ -2,16 +2,64 @@
 "use client";
 
 import { FormFieldTxtT } from "@/common/types/ui";
-import { FieldValues } from "react-hook-form";
+import { ChangeEvent, useCallback } from "react";
+import {
+  Control,
+  Controller,
+  ControllerRenderProps,
+  FieldValues,
+  Path,
+} from "react-hook-form";
 
 type PropsType<T extends FieldValues> = {
   el: FormFieldTxtT<T>;
+  control: Control<T>;
+  cb?: (v: string) => void;
+  isDisabled?: boolean;
 };
 
-const FormFieldTxt = <T extends FieldValues>({ el }: PropsType<T>) => {
+const FormFieldTxt = <T extends FieldValues>({
+  el,
+  control,
+  cb,
+  isDisabled,
+}: PropsType<T>) => {
+  const genDefProps = useCallback(
+    (field: ControllerRenderProps<T, Path<T>>) => ({
+      required: !!el.required,
+      placeholder: el.place,
+      disabled: isDisabled,
+      value: field.value ?? "",
+      className: "input__base bd__sm txt__md",
+
+      onChange: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {
+          target: { value: v },
+        } = e;
+
+        field.onChange(v);
+
+        cb?.(v);
+      },
+    }),
+    [el, cb, isDisabled]
+  );
+
   return (
     <label className="w-full grid grid-cols-1 gap-4">
       {el.label && <span className="txt__lg">{el.label}</span>}
+
+      <Controller
+        name={el.name}
+        control={control}
+        render={({ field }) =>
+          el.type === "textarea" ? (
+            <textarea {...field} {...genDefProps(field)} />
+          ) : (
+            <input {...field} type={el.type} {...genDefProps(field)} />
+          )
+        }
+      />
     </label>
   );
 };
