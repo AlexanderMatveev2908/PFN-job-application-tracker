871b09b0b75816250fdeecd9047e38b0ab26efea
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-10 14:38:44 +0300

    register_page => defined rest of schema

--- apps/client/src/features/auth/schemas/register.ts
+++ apps/client/src/features/auth/schemas/register.ts
@@ -1,12 +1,40 @@
-import { REG_NAME } from "@/core/constants/regex";
+import { REG_NAME, REG_PWD } from "@/core/constants/regex";
 import { z } from "zod";
 
-export const registerSchema = z.object({
-  first_name: z
-    .string()
-    .min(1, "First Name is required")
-    .max(50, "Max length exceeded")
-    .regex(REG_NAME, "Invalid characters"),
-});
+export const registerSchema = z
+  .object({
+    first_name: z
+      .string()
+      .min(1, "First Name is required")
+      .max(50, "Max length exceeded")
+      .regex(REG_NAME, "Invalid characters"),
+    last_name: z
+      .string()
+      .min(1, "Last Name is required")
+      .max(50, "Max length exceeded")
+      .regex(REG_NAME, "Invalid characters"),
+
+    email: z
+      .email({ message: "Invalid email" })
+      .min(1, "Email is required")
+      .max(100, "Max length exceed"),
+
+    pwd: z
+      .string()
+      .min(1, "Password required")
+      .max(100, "Max length exceeded")
+      .regex(REG_PWD, "Invalid password"),
+    confirm_pwd: z.string(),
+
+    terms: z.literal(true, { message: "You must accept terms & conditions" }),
+  })
+  .superRefine((data, ctx) => {
+    if (data.pwd !== data.confirm_pwd)
+      ctx.addIssue({
+        code: "custom",
+        message: "Passwords do not match",
+        path: ["confirm_pwd"],
+      });
+  });
 
 export type RegisterFormT = z.infer<typeof registerSchema>;
