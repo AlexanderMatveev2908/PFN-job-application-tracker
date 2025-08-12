c8bbfa76bec3712e703424ba33b2d5a291b70c9c
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-12 09:50:24 +0300

    pwd_generator => imrpoved flow cpy paste for renredners trigger

--- apps/client/src/common/components/elements/CpyPaste/CpyPaste.tsx
+++ apps/client/src/common/components/elements/CpyPaste/CpyPaste.tsx
@@ -3,47 +3,105 @@
 
 import {
   useEffect,
+  useReducer,
   useRef,
-  useState,
   type CSSProperties,
   type FC,
 } from "react";
 import CpyClip from "./components/CpyClip";
 import { clearTmr } from "@/core/lib/etc";
 import { __cg } from "@/core/lib/log";
+import { reducer } from "./atc/reducer";
+import { initState } from "./atc/initState";
 
 type PropsType = {
   txt: string;
 };
 
 const CpyPaste: FC<PropsType> = ({ txt }) => {
-  const [isCopied, setIsCopied] = useState(false);
+  const [state, dispatchRCT] = useReducer(reducer, initState);
   const btnRef = useRef<HTMLButtonElement | null>(null);
-  const prevCopied = useRef(false);
+  const prevData = useRef({
+    id: "",
+    isCopied: false,
+    forcing: false,
+  });
   const timerID = useRef<NodeJS.Timeout>(null);
 
   useEffect(() => {
     const el = btnRef.current;
     if (!el) return;
 
-    if (isCopied) {
-      prevCopied.current = true;
-      timerID.current = setTimeout(() => {
-        setIsCopied(false);
+    if (
+      !state.isCopied ||
+      prevData.current.id === state.id ||
+      prevData.current.isCopied
+    )
+      return;
 
+    const cb = () => {
+      clearTmr(timerID);
+
+      prevData.current = {
+        ...state,
+        forcing: false,
+      };
+      timerID.current = setTimeout(() => {
+        prevData.current.isCopied = false;
+        dispatchRCT({ type: "CLOSE" });
         clearTmr(timerID);
-      }, 1000);
-    }
+      }, 1500);
+    };
+
+    cb();
 
     return () => {
       clearTmr(timerID);
     };
-  }, [isCopied]);
+  }, [state]);
+
+  useEffect(() => {
+    if (!prevData.current.isCopied || prevData.current.id === state.id) return;
+
+    const cb = () => {
+      clearTmr(timerID);
+      prevData.current = {
+        isCopied: false,
+        id: state.id,
+        forcing: true,
+      };
+
+      dispatchRCT({ type: "CLOSE" });
+    };
+
+    cb();
+  }, [state]);
+
+  useEffect(() => {
+    if (
+      !prevData.current.forcing ||
+      state.isCopied ||
+      state.id === prevData.current.id
+    )
+      return;
+
+    const cb = () => {
+      clearTmr(timerID);
+      prevData.current.forcing = false;
+
+      timerID.current = setTimeout(() => {
+        clearTmr(timerID);
+        dispatchRCT({ type: "FORCE" });
+      }, 300);
+    };
+
+    cb();
+  }, [state]);
 
   const handleClick = async () => {
     try {
       await navigator.clipboard.writeText(txt);
-      setIsCopied(true);
+      dispatchRCT({ type: "OPEN" });
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
       __cg("cpy err", err);
@@ -62,7 +120,7 @@ const CpyPaste: FC<PropsType> = ({ txt }) => {
         } as CSSProperties
       }
     >
-      <CpyClip {...{ isCopied }} />
+      <CpyClip {...{ isCopied: state.isCopied }} />
 
       <span className="txt__md">{txt}</span>
     </button>
