40769102da18855dd65338eb91f6dc1b1afd957c
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-12 16:23:17 +0300

    swap_on_error => improvement swap hook

--- apps/client/src/app/auth/register/page.tsx
+++ apps/client/src/app/auth/register/page.tsx
@@ -6,7 +6,6 @@ import { __cg } from "@/core/lib/log";
 import { FormProvider, Path, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useSwap } from "@/core/hooks/etc/useSwap/useSwap";
-import { useFocusSwap } from "@/core/hooks/ui/useFocusSwap";
 import ProgressSwap from "@/common/components/elements/ProgressSwap";
 import BodyForm from "@/features/auth/pages/register/components/BodyForm";
 import FooterForm from "@/features/auth/pages/register/components/FooterForm";
@@ -20,8 +19,6 @@ import { swapOnErr } from "@/core/lib/forms";
 export type SwapModeT = "swapped" | "swapping" | "none";
 
 const Register: FC = ({}) => {
-  const { startSwap, swapState, lockFocusRef } = useSwap();
-
   const formCtx = useForm<RegisterFormT>({
     mode: "onChange",
     resolver: zodResolver(registerSchema),
@@ -34,8 +31,14 @@ const Register: FC = ({}) => {
       terms: false,
     },
   });
-
   const { setFocus, handleSubmit } = formCtx;
+
+  const kwargs: Path<RegisterFormT>[] = ["first_name", "password"];
+
+  const { startSwap, swapState } = useSwap({
+    setFocus,
+    kwargs,
+  });
 
   const handleSave = handleSubmit(
     (data) => {
@@ -62,15 +65,6 @@ const Register: FC = ({}) => {
       return errs;
     }
   );
-
-  const kwargs: Path<RegisterFormT>[] = ["first_name", "password"];
-
-  useFocusSwap({
-    kwargs,
-    setFocus,
-    swapState,
-    lockFocusRef,
-  });
 
   return (
     <div className="w-full grid grid-cols-1 gap-10 mt-[20px]">
--- apps/client/src/core/hooks/etc/useSwap/useSwap.ts
+++ apps/client/src/core/hooks/etc/useSwap/useSwap.ts
@@ -3,12 +3,25 @@ import { useCallback, useEffect, useReducer, useRef } from "react";
 import { reducer } from "./etc/reducer";
 import { initState, PayloadStartSwapT } from "./etc/initState";
 import { lockFocusCb } from "@/core/lib/style";
+import { useFocusSwap } from "../../ui/useFocusSwap";
+import { FieldValues, Path, UseFormSetFocus } from "react-hook-form";
 
-export const useSwap = () => {
+type Params<T extends FieldValues> = {
+  kwargs: Path<T>[];
+  setFocus: UseFormSetFocus<T>;
+};
+
+export const useSwap = <T extends FieldValues>(params: Params<T>) => {
   const [state, dispatchRCT] = useReducer(reducer, initState);
   const timerWapMode = useRef<NodeJS.Timeout | null>(null);
-
   const lockFocusRef = useRef<boolean>(false);
+
+  useFocusSwap({
+    kwargs: params.kwargs,
+    setFocus: params.setFocus,
+    swapState: state,
+    lockFocusRef,
+  });
 
   const startSwap = useCallback(
     ({ swap, lockFocus }: PayloadStartSwapT & { lockFocus?: boolean }) => {
