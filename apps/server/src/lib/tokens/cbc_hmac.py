f1d08674b40af4d70056e4cd6b2b8d9805d0c64f
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-15 19:01:04 +0300

    cbc_hmac => cbc alg works

--- apps/server/src/lib/tokens/cbc_hmac.py
+++ apps/server/src/lib/tokens/cbc_hmac.py
@@ -1,5 +1,5 @@
-from __future__ import annotations
-
+import binascii
+import json
 import os
 import hmac
 from typing import Tuple
@@ -26,7 +26,7 @@ def gen_aes_cbc(k: bytes, plain_txt: bytes) -> Tuple[bytes, bytes]:
     return iv, ct
 
 
-def aes_cbc_decrypt(k: bytes, iv: bytes, ciphertext: bytes) -> bytes:
+def dec_aes_cbc(k: bytes, iv: bytes, ciphertext: bytes) -> bytes:
     cipher = Cipher(algorithms.AES(k), modes.CBC(iv))
     dec = cipher.decryptor()
     padded = dec.update(ciphertext) + dec.finalize()
@@ -39,7 +39,7 @@ def aes_cbc_decrypt(k: bytes, iv: bytes, ciphertext: bytes) -> bytes:
     return pt
 
 
-def hmac_sha256(
+def gen_sha256(
     mac_key: bytes, *, aad: bytes, iv: bytes, ciphertext: bytes
 ) -> bytes:
     h = chmac.HMAC(mac_key, hashes.SHA256())
@@ -51,3 +51,34 @@ def hmac_sha256(
 
 def constant_time_eq(a: bytes, b: bytes) -> bool:
     return hmac.compare_digest(a, b)
+
+
+CBC_K = "ad79b2e28912c7bf764c580ad44e0e315027f02738273f2379cc0b72739467c7"
+SHA_K = "d231c215ed1350eb0f802cf2b30165af8334cf1b9c5ddc8ea50e6d0c33c39f6a"
+
+
+payload = json.dumps({"id": 12345})
+
+
+def handle_0() -> None:
+    tup = gen_aes_cbc(binascii.unhexlify(CBC_K), payload.encode("utf-8"))
+
+    for x in tup:
+        print(x.hex())
+
+
+RES_IV = "90d8d506b900ca7ed0d22514feb19b78"
+RES_CT = "b0470d45951aa83b6e20e7e7766a966a"
+
+
+def handle_1() -> None:
+    pt = dec_aes_cbc(
+        binascii.unhexlify(CBC_K),
+        binascii.unhexlify(RES_IV),
+        binascii.unhexlify(RES_CT),
+    )
+
+    print(pt.decode("utf-8"))
+
+
+handle_1()
