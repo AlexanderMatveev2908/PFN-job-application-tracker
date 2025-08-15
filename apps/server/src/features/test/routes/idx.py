9be9103e983ed13c8ed7a29659826efa62822859
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-15 07:46:48 +0300

    main => removed limiter test route

--- apps/server/src/features/test/routes/idx.py
+++ apps/server/src/features/test/routes/idx.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from src.features.test.controllers.get import (
     get_msg_ctrl,
 )
-from src.middleware.rate_limiter import rate_limit
 from ..controllers.post import (
     post_form_ctrl,
     post_msg_ctrl,
@@ -11,6 +10,8 @@ from ..controllers.post import (
 router_test = APIRouter(prefix="/test")
 
 
-router_test.get("/", dependencies=[Depends(rate_limit())])(get_msg_ctrl)
+router_test.get(
+    "/",
+)(get_msg_ctrl)
 router_test.post("/")(post_msg_ctrl)
 router_test.post("/form")(post_form_ctrl)
