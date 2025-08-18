5668588a12ff41ab05aa8f7d3fd077cca984b2e6
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-18 12:36:42 +0300

    main => _

--- apps/server/tests/conf/constants.py
+++ apps/server/tests/conf/constants.py
@@ -4,7 +4,7 @@ import os
 PAYLOAD_REGISTER = {
     "first_name": "John",
     "last_name": "Doe",
-    "email": f"{os.urandom(16).hex()}@gmail.com",
+    "email": f"{os.urandom(50).hex()}@gmail.com",
     "password": "qcxd-0KK64BVu5Z84DY5@czE6{!^zI_a2e9^{P(#",
     "confirm_password": "qcxd-0KK64BVu5Z84DY5@czE6{!^zI_a2e9^{P(#",
     "terms": True,
