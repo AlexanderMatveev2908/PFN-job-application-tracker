e7ba72528c5f65a227d93313b15e235baf2fc457
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-19 21:11:20 +0300

    verify_forgot_pwd_token => finished tests check forgot pwd token

--- apps/server/tests/routes/verify/forgot_pwd_test.py
+++ apps/server/tests/routes/verify/forgot_pwd_test.py
@@ -27,5 +27,43 @@ async def ok_t(api: AsyncClient) -> None:
 
 
 @pytest.mark.asyncio
-async def not_found_t(api: AsyncClient) -> None:
-    await wrap_httpx(api, url=f"{URL}", expected_code=401, method="GET")
+async def err_not_provided_t(api: AsyncClient) -> None:
+    res_check = await wrap_httpx(
+        api, url=f"{URL}", expected_code=401, method="GET"
+    )
+
+    assert "CBC_HMAC_NOT_PROVIDED" in res_check["data"]["msg"]
+
+
+@pytest.mark.asyncio
+async def err_invalid_t(api: AsyncClient) -> None:
+    res_tokens = await get_tokens_lib(
+        api,
+        cbc_hmac_t=TokenT.RECOVER_PWD,
+    )
+
+    res_check = await wrap_httpx(
+        api,
+        url=f"{URL}{'hack' + res_tokens['cbc_hmac_token'][4:]}",
+        expected_code=401,
+        method="GET",
+    )
+
+    assert "CBC_HMAC_INVALID_FORMAT" in res_check["data"]["msg"]
+
+
+@pytest.mark.asyncio
+async def err_wrong_token_type_t(api: AsyncClient) -> None:
+    res_tokens = await get_tokens_lib(
+        api,
+        cbc_hmac_t=TokenT.CHANGE_PWD,
+    )
+
+    res_check = await wrap_httpx(
+        api,
+        url=f"{URL}{res_tokens['cbc_hmac_token']}",
+        expected_code=401,
+        method="GET",
+    )
+
+    assert "CBC_HMAC_WRONG_TYPE" in res_check["data"]["msg"]
