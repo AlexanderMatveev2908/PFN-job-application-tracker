f3a5176500780cfaf19b875d7a7ffaa089413d5b
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-19 14:16:41 +0300

    tests_require_email_forgot_pwd => improvement tests

--- apps/server/src/middleware/check_token.py
+++ apps/server/src/middleware/check_token.py
@@ -1,3 +1,4 @@
+from typing import Awaitable, Callable
 from fastapi import Request
 from src.conf.db import db_trx
 from src.constants.reg import REG_CBC_HMAC
@@ -6,18 +7,22 @@ from src.lib.tokens.cbc_hmac import check_cbc_hmac
 from src.models.token import CheckTokenReturnT, TokenT
 
 
-async def check_cbc_hmac_mdw(
-    req: Request, token_t: TokenT = TokenT.CONF_EMAIL
-) -> CheckTokenReturnT:
-    token = req.query_params.get("cbc_hmac_token", None)
+def check_cbc_hmac_mdw(
+    token_t: TokenT = TokenT.CONF_EMAIL,
+) -> Callable[[Request], Awaitable[CheckTokenReturnT]]:
 
-    if not token:
-        raise ErrAPI(msg="CBC_HMAC_NOT_PROVIDED", status=401)
+    async def _check_cbc(req: Request) -> CheckTokenReturnT:
+        token = req.query_params.get("cbc_hmac_token", None)
 
-    if not REG_CBC_HMAC.fullmatch(token):
-        raise ErrAPI(msg="CBC_HMAC_INVALID_FORMAT", status=401)
+        if not token:
+            raise ErrAPI(msg="CBC_HMAC_NOT_PROVIDED", status=401)
 
-    async with db_trx(auto_commit=False) as trx:
-        return await check_cbc_hmac(
-            token, trx, commit_soft_delete=True, token_t=token_t
-        )
+        if not REG_CBC_HMAC.fullmatch(token):
+            raise ErrAPI(msg="CBC_HMAC_INVALID_FORMAT", status=401)
+
+        async with db_trx(auto_commit=False) as trx:
+            return await check_cbc_hmac(
+                token, trx, commit_soft_delete=True, token_t=token_t
+            )
+
+    return _check_cbc
