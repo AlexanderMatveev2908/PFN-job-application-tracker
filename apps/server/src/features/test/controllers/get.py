e650755c21de7f5b35c0416d15075c5e506ebc6b
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-20 14:52:03 +0300

    build_protected_endpoint => wrote base endpoint

--- apps/server/src/features/test/controllers/get.py
+++ apps/server/src/features/test/controllers/get.py
@@ -7,3 +7,7 @@ async def get_msg_ctrl(req: Request) -> ResAPI:
     return ResAPI.ok_200(
         msg="✅ get request ☎️",
     )
+
+
+async def get_protected_data_ctrl(req: Request) -> ResAPI:
+    return ResAPI.ok_200()
--- apps/server/src/features/test/routes/idx.py
+++ apps/server/src/features/test/routes/idx.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.features.test.controllers.get import (
     get_msg_ctrl,
+    get_protected_data_ctrl,
 )
+from src.middleware.rate_limiter import rate_limit_mdw
 from ..controllers.post import (
     get_err_ctrl,
     post_form_ctrl,
@@ -17,9 +19,14 @@ router_test.get(
     "/",
 )(get_msg_ctrl)
 router_test.post("/")(post_msg_ctrl)
+
 router_test.post("/form")(post_form_ctrl)
 
 router_test.post("/tokens-health")(tokens_health_ctrl)
 router_test.post("/get-tokens-expired")(tokens_expired_ctrl)
 router_test.post("/get-err-expired")(get_err_ctrl)
 router_test.post("/get-err-invalid")(get_err_ctrl)
+
+router_test.get(
+    "/protected-data", dependencies=[Depends(rate_limit_mdw(limit=5))]
+)(get_protected_data_ctrl)
