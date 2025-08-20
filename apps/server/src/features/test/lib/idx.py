681d78135b3e40325419ea65408b17089788264e
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-20 16:44:33 +0300

    access_refresh_tests => _

--- apps/server/src/features/test/lib/idx.py
+++ apps/server/src/features/test/lib/idx.py
@@ -1,6 +1,17 @@
 from fastapi import Request
+from src.decorators.err import ErrAPI
 from src.models.token import TokenT
 
 
 def get_query_token_t(req: Request) -> TokenT:
-    return TokenT(req.query_params.get("cbc_hmac_token_t", "CONF_EMAIL"))
+
+    token_t = req.query_params.get("cbc_hmac_token_t")
+
+    if not token_t:
+        raise ErrAPI(msg="token type not provided", status=400)
+
+    try:
+        parsed = TokenT(token_t)
+        return parsed
+    except Exception:
+        raise ErrAPI(msg="invalid token type", status=400)
