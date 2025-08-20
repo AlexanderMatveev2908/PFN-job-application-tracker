9e78ffa59817b3b18d54fbec83207a6e0866286e
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-20 11:28:00 +0300

    tests_change_pwd => tests bas case passed

--- apps/server/tests/routes/auth/change_pwd_test.py
+++ apps/server/tests/routes/auth/change_pwd_test.py
@@ -31,3 +31,69 @@ async def ok_t(api: AsyncClient) -> None:
     assert REG_JWT.fullmatch(res_change["data"]["access_token"])
     assert REG_JWE.fullmatch(res_change["refresh_token"])
     assert "password updated" in res_change["data"]["msg"]
+
+
+@pytest.mark.asyncio
+async def err_same_pwd_t(api: AsyncClient) -> None:
+    res_tokens = await get_tokens_lib(
+        api,
+        cbc_hmac_t=TokenT.RECOVER_PWD,
+    )
+
+    res_change = await wrap_httpx(
+        api,
+        url=URL,
+        data={
+            "cbc_hmac_token": res_tokens["cbc_hmac_token"],
+            "new_password": res_tokens["payload"]["password"],
+        },
+        expected_code=400,
+    )
+
+    assert (
+        "new password must be different from old one"
+        in res_change["data"]["msg"]
+    )
+
+
+@pytest.mark.asyncio
+async def err_expired_t(api: AsyncClient) -> None:
+    res_tokens = await get_tokens_lib(
+        api, cbc_hmac_t=TokenT.RECOVER_PWD, reverse=True
+    )
+
+    new_pwd = gen_pwd(n=5)
+
+    res_change = await wrap_httpx(
+        api,
+        url=URL,
+        data={
+            "cbc_hmac_token": res_tokens["cbc_hmac_token"],
+            "new_password": new_pwd,
+        },
+        expected_code=401,
+    )
+
+    assert "CBC_HMAC_EXPIRED" in res_change["data"]["msg"]
+
+
+@pytest.mark.asyncio
+async def invalid_action_t(api: AsyncClient) -> None:
+    res_tokens = await get_tokens_lib(
+        api,
+        cbc_hmac_t=TokenT.MANAGE_ACC,
+    )
+
+    new_pwd = gen_pwd(n=5)
+
+    res_change = await wrap_httpx(
+        api,
+        url=URL,
+        data={
+            "cbc_hmac_token": res_tokens["cbc_hmac_token"],
+            "new_password": new_pwd,
+        },
+        expected_code=401,
+    )
+
+    assert "CBC_HMAC_WRONG_TYPE" in res_change["data"]["msg"]
