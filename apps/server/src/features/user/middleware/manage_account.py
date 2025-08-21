e99abcb4cda4f533a9a2bcf4bc7a1324b821a4ca
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-21 11:35:44 +0300

    get_access_account => finished endpoint get_access_to_manage_account

--- apps/server/src/features/user/middleware/manage_account.py
+++ apps/server/src/features/user/middleware/manage_account.py
@@ -1,10 +1,22 @@
 from fastapi import Request
+from src.decorators.err import ErrAPI
+from src.lib.hashing.idx import check_pwd
 from src.lib.validators.idx import PwdFormT
+from src.middleware.check_form import check_form_mdw
+from src.middleware.check_jwt import check_check_jwt_mdw
+from src.models.user import UserDcT
 
 
 class ManageAccountForm(PwdFormT):
     pass
 
 
-async def manage_account(req: Request) -> None:
-    pass
+async def manage_account_mdw(req: Request) -> UserDcT:
+    us: UserDcT = await check_check_jwt_mdw(req)
+
+    result = await check_form_mdw(ManageAccountForm, req)
+
+    if not (await check_pwd(hashed=us["password"], plain=result.password)):
+        raise ErrAPI(msg="invalid password", status=401)
+
+    return us
