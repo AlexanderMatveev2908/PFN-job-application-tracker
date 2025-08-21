dd94dd1b2a2fc16371efb63ff4ac9e4f7cd36ed4
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-21 10:20:51 +0300

    get_access_account => reduced boilerplate pwd

--- apps/server/src/features/auth/middleware/recover_pwd.py
+++ apps/server/src/features/auth/middleware/recover_pwd.py
@@ -1,25 +1,19 @@
 from typing import TypedDict, cast
 from fastapi import Request
-from pydantic import BaseModel, Field, field_validator
+from pydantic import Field, field_validator
 
 from src.conf.db import db_trx
 from src.lib.tokens.cbc_hmac import check_cbc_hmac
 from src.lib.validators.idx import (
+    PwdFormT,
     check_basic_cbc_shape_lib,
-    validate_password_lib,
 )
 from src.middleware.check_form import check_form_mdw
 from src.models.token import CheckTokenReturnT, TokenT
 
 
-class RecoverPwdFrom(BaseModel):
-    new_password: str = Field(min_length=1, max_length=100)
+class RecoverPwdFrom(PwdFormT):
     cbc_hmac_token: str | None = Field(default=None, validate_default=True)
-
-    @field_validator("new_password")
-    def _validate_password(cls, v: str) -> str:
-
-        return validate_password_lib(v)
 
     @field_validator("cbc_hmac_token")
     def _check_token(cls, v: str) -> str:
@@ -39,7 +33,7 @@ async def recover_pwd_mdw(req: Request) -> RecoverPwdMdwReturnT:
     async with db_trx() as trx:
 
         return {
-            "new_password": data.new_password,
+            "new_password": data.password,
             "check_cbc_hmac_result": await check_cbc_hmac(
                 trx=trx,
                 token_t=TokenT.RECOVER_PWD,
