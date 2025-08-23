d8828409244d25235f7d875ff35bcb2defddac14
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-23 08:47:42 +0300

    change_pwd => passed with no regression

--- apps/server/src/features/user/controllers/patch.py
+++ apps/server/src/features/user/controllers/patch.py
@@ -1,12 +1,20 @@
 from fastapi import Depends, Request
 
 from src.decorators.res import ResAPI
-from src.features.user.middleware.action_account import ActionAccountMdwReturnT
-from src.features.user.middleware.change_pwd import change_pwd_mdw
+from src.lib.validators.idx import PwdFormT
+from src.middleware.combo.idx import (
+    ComboCheckCbJwtCbcReturnT,
+    combo_check_bd_jwt_bcb_hmac_mdw,
+)
+from src.models.token import TokenT
 
 
 async def change_pwd_ctrl(
-    req: Request,
-    result_action_mdw: ActionAccountMdwReturnT = Depends(change_pwd_mdw),
+    _: Request,
+    result_action_mdw: ComboCheckCbJwtCbcReturnT = Depends(
+        combo_check_bd_jwt_bcb_hmac_mdw(
+            model=PwdFormT, token_t=TokenT.MANAGE_ACC, check_jwt=True
+        )
+    ),
 ) -> ResAPI:
     return ResAPI.ok_200(**result_action_mdw)
--- apps/server/src/middleware/check_cbc_hmac.py
+++ apps/server/src/middleware/check_cbc_hmac.py
@@ -1,14 +1,9 @@
-from typing import Any, Awaitable, Callable, Type, TypeVar, TypedDict, cast
+from typing import Awaitable, Callable, cast
 from fastapi import Request
-from pydantic import BaseModel
 from src.conf.db import db_trx
-from src.decorators.err import ErrAPI
 from src.lib.tokens.cbc_hmac import check_cbc_hmac
 from src.lib.validators.idx import CbcHmacFormT
-from src.middleware.check_form import check_form_mdw
 from src.models.token import CheckTokenReturnT, TokenT
-
-FormT = TypeVar("FormT", bound=BaseModel)
 
 
 def check_cbc_hmac_mdw(
@@ -24,35 +19,3 @@ def check_cbc_hmac_mdw(
             return await check_cbc_hmac(cast(str, token), trx, token_t=token_t)
 
     return _check_cbc
-
-
-class CheckReqCbcHmacMdwReturnT(TypedDict):
-    body: dict[str, Any]
-    cbc_hmac_result: CheckTokenReturnT
-
-
-def check_req_and_cbc_hmac_mdw(
-    model: Type[FormT], token_t: TokenT
-) -> Callable[[Request], Awaitable[CheckReqCbcHmacMdwReturnT]]:
-
-    async def _check(req: Request) -> CheckReqCbcHmacMdwReturnT:
-        bd: dict | None = None
-
-        try:
-            bd = await req.json()
-        except Exception:
-            ...
-
-        if not bd:
-            raise ErrAPI(msg="Invalid data format", status=422)
-
-        async with db_trx() as trx:
-            result_cbc_hmac = await check_cbc_hmac(
-                token=bd["cbc_hmac_token"], trx=trx, token_t=token_t
-            )
-
-            await check_form_mdw(model=model, data=bd)
-
-            return {"body": bd, "cbc_hmac_result": result_cbc_hmac}
-
-    return _check
