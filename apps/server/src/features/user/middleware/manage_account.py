9c4aaac618d77139cd0ec92374563982530e4cde
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-23 08:32:49 +0300

    change_pwd => checkpoint

--- apps/server/src/features/user/middleware/manage_account.py
+++ apps/server/src/features/user/middleware/manage_account.py
@@ -7,16 +7,12 @@ from src.middleware.check_jwt import check_jwt_search_us_mdw
 from src.models.user import UserDcT
 
 
-class ManageAccountForm(PwdFormT):
-    pass
-
-
 async def manage_account_mdw(req: Request) -> UserDcT:
     us: UserDcT = await check_jwt_search_us_mdw(
         req,
     )
 
-    result = await check_form_mdw(ManageAccountForm, req)
+    result = await check_form_mdw(PwdFormT, req)
 
     if not (await check_pwd(hashed=us["password"], plain=result.password)):
         raise ErrAPI(msg="invalid password", status=401)
