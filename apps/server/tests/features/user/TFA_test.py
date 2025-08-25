41c3ef5c9d35f4e5aa396422d441cd1eee308230
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-25 16:32:24 +0300

    tests_2FA => tested good case

--- apps/server/tests/features/user/TFA_test.py
+++ apps/server/tests/features/user/TFA_test.py
@@ -1,9 +1,26 @@
 from httpx import AsyncClient
 import pytest
 
+from src.constants.reg import REG_SECRET_TOTP
 from tests.conf.lib.etc import gen_verified_user_lib
+from tests.conf.lib.idx import wrap_httpx
+
+URL = "/user/2FA"
 
 
 @pytest.mark.asyncio
 async def ok_t(api: AsyncClient) -> None:
     res = await gen_verified_user_lib(api)
+
+    res_2FA = await wrap_httpx(
+        api,
+        url=URL,
+        method="PATCH",
+        access_token=res["access_token"],
+        data={"cbc_hmac_token": res["cbc_hmac_token"]},
+        expected_code=200,
+    )
+
+    assert REG_SECRET_TOTP.fullmatch(res_2FA["data"]["totp_secret"])
+
+    assert len(res_2FA["data"]["backup_codes"]) == 8
