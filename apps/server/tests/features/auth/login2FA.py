a076003cf4b6da2da46cdd714ae8d4bd23380a9e
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-26 08:33:38 +0300

    tests_login_2FA => passed good case

--- apps/server/tests/features/auth/login2FA.py
+++ apps/server/tests/features/auth/login2FA.py
@@ -1,7 +1,8 @@
 from httpx import AsyncClient
+import pyotp
 import pytest
 
-from src.constants.reg import REG_CBC_HMAC
+from src.constants.reg import REG_CBC_HMAC, REG_JWE, REG_JWT
 from src.lib.data_structure import b_to_d, h_to_b
 from src.models.token import TokenT
 from tests.conf.lib.etc import get_user_2FA
@@ -29,3 +30,16 @@ async def ok_t(api: AsyncClient) -> None:
         TokenT(b_to_d(h_to_b(cbc_hmac_token.split(".")[0]))["token_t"])
         == TokenT.LOGIN_2FA
     )
+
+    res_login_totp = await wrap_httpx(
+        api,
+        url="/auth/login-totp",
+        data={
+            "totp_code": pyotp.TOTP(res_2FA["totp_secret"]).now(),
+            "cbc_hmac_token": res_login["data"]["cbc_hmac_token"],
+        },
+        expected_code=200,
+    )
+
+    assert REG_JWT.fullmatch(res_login_totp["data"]["access_token"])
+    assert REG_JWE.fullmatch(res_login_totp["refresh_token"])
