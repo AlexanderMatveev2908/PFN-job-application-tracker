d3b7aa706d0a441acb09074bd34eb68bf6a75708
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-26 12:33:05 +0300

    manage_account_2fa => passed bad cases

--- apps/server/tests/features/user/manage_account_2fa_test.py
+++ apps/server/tests/features/user/manage_account_2fa_test.py
@@ -2,6 +2,7 @@ from httpx import AsyncClient
 import pytest
 from src.models.token import TokenT
 from tests.conf.lib.data_structure import gen_totp, get_aad_cbc_hmac
+from tests.conf.lib.etc import get_tokens_lib
 from tests.conf.lib.idx import wrap_httpx
 from tests.conf.lib.login import get_logged_2fa
 
@@ -56,3 +57,48 @@ async def ok_t(api: AsyncClient) -> None:
     )
 
     assert "user account deleted" in res_delete["data"]["msg"]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "case, expected_code, expected_msg",
+    [
+        ("cbc_hmac_invalid", 401, "cbc_hmac_invalid"),
+        ("cbc_hmac_expired", 401, "cbc_hmac_expired"),
+        ("totp_invalid", 401, "totp_code_invalid"),
+    ],
+)
+async def base_cases_t(
+    api: AsyncClient, case: str, expected_code: int, expected_msg: str
+) -> None:
+    res_logged = await get_logged_2fa(api)
+
+    res_tokens = await get_tokens_lib(
+        api,
+        cbc_hmac_t=TokenT.MANAGE_ACC_2FA,
+        existing_payload=res_logged["payload"],
+        expired=case.split("_expired"),
+    )
+
+    totp_code = (
+        "123456"
+        if case == "totp_invalid"
+        else gen_totp(res_logged["totp_secret"])
+    )
+
+    token_2fa = res_tokens["cbc_hmac_token"]
+    if case == "cbc_hmac_invalid":
+        token_2fa = token_2fa[:-4] + "a2fa"
+
+    res_totp = await wrap_httpx(
+        api,
+        url="/user/manage-account-2FA",
+        access_token=res_logged["access_token"],
+        data={
+            "totp_code": totp_code,
+            "cbc_hmac_token": token_2fa,
+        },
+        expected_code=expected_code,
+    )
+
+    assert expected_msg in res_totp["data"]["msg"]
