5d40ba7c0e7a37e60348122dbd9711c74ba80657
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-27 18:06:00 +0300

    🪾 general_refactor => 🧪 added bad case confirm new email 2fa totp

--- apps/server/tests/features/verify/confirm_new_email_2FA_totp_test.py
+++ apps/server/tests/features/verify/confirm_new_email_2FA_totp_test.py
@@ -71,3 +71,57 @@ async def ok_t(api: AsyncClient) -> None:
     )
 
     assrt_sessions_tokens(res_verify_1)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "case, expected_code, expected_msg",
+    [("wrong_code", 401, "totp_code_invalid")],
+)
+async def bad_cases_t(
+    api: AsyncClient, case: str, expected_code: int, expected_msg: str
+) -> None:
+    res_2fa = await get_us_2FA_lib(api)
+
+    new_email = faker.Faker().email()
+
+    res_change = await wrap_httpx(
+        api,
+        url="/user/change-email",
+        method="PATCH",
+        expected_code=200,
+        data={
+            "email": new_email,
+            "cbc_hmac_token": res_2fa["cbc_hmac_token"],
+        },
+        access_token=res_2fa["access_token"],
+    )
+
+    assrt_msg(res_change, "email sent")
+
+    res_verify = await wrap_httpx(
+        api,
+        url=f"/verify/new-email?cbc_hmac_token={(await get_tokens_lib(api, existing_payload=res_2fa["payload"], cbc_hmac_t=TokenT.CHANGE_EMAIL))['cbc_hmac_token']}",  # noqa: E501
+        method="GET",
+    )
+
+    token_2fa = grab(res_verify, "cbc_hmac_token")
+
+    get_aad_cbc_hmac(token_2fa, TokenT.CHANGE_EMAIL_2FA)
+
+    res_totp = await wrap_httpx(
+        api,
+        url="/verify/new-email-2FA-totp",
+        method="PATCH",
+        data={
+            "cbc_hmac_token": token_2fa,
+            "totp_code": (
+                "123456"
+                if case == "wrong_code"
+                else gen_totp(res_2fa["totp_secret"])
+            ),
+        },
+        expected_code=expected_code,
+    )
+
+    assrt_msg(res_totp, expected_msg)
