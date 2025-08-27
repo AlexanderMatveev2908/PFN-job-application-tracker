e2143155a7037ae7f38826572b6902499121ab2f
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-27 14:59:11 +0300

    🪾 tests_recover_pwd_2FA_backup_code => 🧪 passed bad cases

--- apps/server/tests/features/verify/recover_pwd_2FA_backup_code_test.py
+++ apps/server/tests/features/verify/recover_pwd_2FA_backup_code_test.py
@@ -5,27 +5,21 @@ from src.lib.etc import grab
 from src.lib.pwd_gen import gen_pwd
 from src.models.token import TokenT
 from tests.conf.lib.data_structure import (
+    assrt_msg,
     assrt_sessions_tokens,
     get_aad_cbc_hmac,
 )
-from tests.conf.lib.etc import get_tokens_lib
 from tests.conf.lib.get_us import get_us_2FA_lib
 from tests.conf.lib.idx import wrap_httpx
 
 
 @pytest.mark.asyncio
 async def ok_t(api: AsyncClient) -> None:
-    res_logged = await get_us_2FA_lib(api)
-
-    res_tokens = await get_tokens_lib(
-        api,
-        existing_payload=res_logged["payload"],
-        cbc_hmac_t=TokenT.RECOVER_PWD,
-    )
+    res_logged = await get_us_2FA_lib(api, cbc_hmac_t=TokenT.RECOVER_PWD)
 
     res_verify = await wrap_httpx(
         api,
-        url=f"/verify/recover-pwd?cbc_hmac_token={res_tokens['cbc_hmac_token']}",  # noqa: E501
+        url=f"/verify/recover-pwd?cbc_hmac_token={res_logged['cbc_hmac_token']}",  # noqa: E501
         expected_code=200,
         method="GET",
     )
@@ -55,3 +49,60 @@ async def ok_t(api: AsyncClient) -> None:
     )
 
     assrt_sessions_tokens(res_new_pwd)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "case, expected_code, expected_msg",
+    [
+        ("wrong_code", 401, "backup_code_invalid"),
+        ("wrong_endpoint", 403, "user must follow 2fa flow"),
+    ],
+)
+async def bad_cases_t(
+    api: AsyncClient, case: str, expected_code: int, expected_msg: str
+) -> None:
+    res_2fa = await get_us_2FA_lib(
+        api,
+        cbc_hmac_t=TokenT.RECOVER_PWD,
+    )
+
+    if case == "wrong_endpoint":
+        res_change = await wrap_httpx(
+            api,
+            url="/auth/recover-pwd",
+            data={
+                "password": gen_pwd(5),
+                "cbc_hmac_token": res_2fa["cbc_hmac_token"],
+            },
+            method="PATCH",
+            expected_code=expected_code,
+        )
+
+        assrt_msg(res_change, expected_msg)
+        return
+
+    res_verify = await wrap_httpx(
+        api,
+        url=f'/verify/recover-pwd?cbc_hmac_token={res_2fa["cbc_hmac_token"]}',
+        method="GET",
+        expected_code=200,
+    )
+
+    token_2fa = grab(res_verify, "cbc_hmac_token")
+
+    res_backup = await wrap_httpx(
+        api,
+        url="/verify/recover-pwd-2FA-backup-code",
+        expected_code=expected_code,
+        data={
+            "cbc_hmac_token": token_2fa,
+            "backup_code": (
+                "1234-5678"
+                if case == "wrong_code"
+                else res_2fa["backup_codes"][0]
+            ),
+        },
+    )
+
+    assrt_msg(res_backup, expected_msg)
