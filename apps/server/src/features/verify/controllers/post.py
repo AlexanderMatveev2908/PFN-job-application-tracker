793a44bc0ad78fb0a916bd68572f62a82b6c23c3
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-27 13:21:06 +0300

    recover_pwd_2FA_backup_code => validated endpoint

--- apps/server/src/features/verify/controllers/post.py
+++ apps/server/src/features/verify/controllers/post.py
@@ -3,6 +3,7 @@ from fastapi import Depends, Request
 from src.conf.db import db_trx
 from src.decorators.err import ErrAPI
 from src.decorators.res import ResAPI
+from src.features.auth.middleware.login_backup_code import BackupCodeFormT
 from src.features.auth.middleware.login_totp import TotpFormT
 from src.lib.db.idx import get_us_by_id
 from src.lib.etc import grab
@@ -31,5 +32,14 @@ async def forgot_pwd_2FA_totp_ctrl(
         return ResAPI.ok_200(msg="verification successful")
 
 
-async def forgot_pwd_2FA_backup_code_ctrl(req: Request) -> ResAPI:
+async def forgot_pwd_2FA_backup_code_ctrl(
+    req: Request,
+    res_combo: ComboCheckJwtCbcBodyReturnT = Depends(
+        combo_check_jwt_cbc_hmac_body_mdw(
+            check_jwt=False,
+            model=BackupCodeFormT,
+            token_t=TokenT.RECOVER_PWD_2FA,
+        )
+    ),
+) -> ResAPI:
     return ResAPI.ok_200()
