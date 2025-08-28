f967023eb9f7c82bff5080998b57aafb32df34e8
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-28 20:57:03 +0300

    🪾 main => ⚠️ update timeout tooltips

--- apps/client/tests/e2e/auth/register/form/swap._0.test.ts
+++ apps/client/tests/e2e/auth/register/form/swap._0.test.ts
@@ -13,13 +13,15 @@ test("swap 0", async ({ page }) => {
 
   const msgs = ["invalid characters", "last name is required", "invalid email"];
 
+  await page.waitForTimeout(1000);
+
   await checkTxtList(page, msgs);
 
   await el.getByTestId("first_name").fill("John");
   await el.getByTestId("last_name").fill("Doe");
   await el.getByTestId("email").fill("john@gmail.com");
 
-  await page.waitForTimeout(500);
+  await page.waitForTimeout(1000);
 
   await checkTxtListOpc(page, msgs);
 });
