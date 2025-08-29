909e60070ec3660baa3affe7dc61c7f76e8ad069
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-29 18:59:00 +0300

    🪾 get_user_data => 🔥 validated enpoint

--- apps/server/src/features/user/controllers/get.py
+++ apps/server/src/features/user/controllers/get.py
@@ -1,8 +1,13 @@
-from fastapi import Request
+from fastapi import Depends, Request
 from fastapi.responses import JSONResponse
 
 from src.decorators.res import ResAPI
+from src.middleware.check_jwt import check_jwt_search_us_mdw
+from src.models.user import UserDcT
 
 
-async def get_us_profile_ctrl(req: Request) -> JSONResponse:
-    return ResAPI(req).ok_200()
+async def get_us_profile_ctrl(
+    req: Request, us: UserDcT = Depends(check_jwt_search_us_mdw)
+) -> JSONResponse:
+
+    return ResAPI(req).ok_200(ur=us)
