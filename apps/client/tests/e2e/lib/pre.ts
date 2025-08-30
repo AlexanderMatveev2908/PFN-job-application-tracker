f132cd0d881ffcef9635b2441ee4ba4d697bb9ef
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-08-30 14:13:23 +0300

    🪾 refactor_client_tests => 🛠️ _

--- apps/client/tests/e2e/lib/pre.ts
+++ apps/client/tests/e2e/lib/pre.ts
@@ -4,5 +4,5 @@ import { waitTest } from "./sideActions";
 export const preTest = async (page: Page, url: string) => {
   await page.goto(url);
 
-  await waitTest(page, 1000);
+  await waitTest(page, 1500);
 };
