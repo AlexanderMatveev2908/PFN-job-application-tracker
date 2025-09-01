3cdae53254dd602f46959093a01cc1555f4f10cf
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-09-01 09:46:26 +0300

    🪾 tests_client_require_email_conf_email => 🧪 testes passed

---
+++ apps/client/tests/e2e/requireEmail/err.test.ts
@@ -0,0 +1,27 @@
+import test from "@playwright/test";
+import { preRequireEmail } from "./pre";
+import { getByID, getByTxt } from "../lib/get";
+import { clickByID } from "../lib/click";
+import { waitTest } from "../lib/sideActions";
+
+test("invalid email", async ({ browser }) => {
+  const { form, pageRequire: page } = await preRequireEmail(browser);
+
+  await (await getByID(form, "email")).fill("<>@<>");
+
+  await waitTest(page);
+
+  await getByTxt(page, "invalid email");
+});
+
+test("non existent", async ({ browser }) => {
+  const { form, pageRequire: page, payload } = await preRequireEmail(browser);
+
+  await (await getByID(form, "email")).fill(payload.email);
+
+  await clickByID(form, "conf_email__footer_form__submit_btn");
+
+  await waitTest(page);
+
+  await getByTxt(page, "user not found");
+});
---
+++ apps/client/tests/e2e/requireEmail/ok.test.ts
@@ -0,0 +1,26 @@
+import test from "@playwright/test";
+import { preRequireEmail } from "./pre";
+import { getByID, getByTxt } from "../lib/get";
+import { clickByID } from "../lib/click";
+import { waitTest, waitURL } from "../lib/sideActions";
+import { genMailNoticeMsg } from "@/core/constants/etc";
+
+test("ok", async ({ browser }) => {
+  const {
+    form,
+    pageRequire: page,
+    payload,
+  } = await preRequireEmail(browser, true);
+
+  await (await getByID(form, "email")).fill(payload.email);
+
+  await clickByID(form, "conf_email__footer_form__submit_btn");
+
+  await waitURL(page, "/notice");
+
+  await waitTest(page);
+
+  await getByTxt(page, genMailNoticeMsg("to confirm the account"));
+
+  await getByTxt(page, "email sent");
+});
