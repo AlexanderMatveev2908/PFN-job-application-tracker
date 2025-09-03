0947002f2cc91d95a33d5e4f95040fa1ae72c150
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-09-03 17:12:55 +0300

    🪾 tests_del_account => 🛠️ good case test passed

---
+++ apps/client/tests/e2e/user/manageAcc/delAcc/ok.test.ts
@@ -0,0 +1,27 @@
+import test from "@playwright/test";
+import { clickByID, getByID, isToastOk } from "../../../lib/idx";
+import { waitTmr, waitURL } from "../../../lib/shortcuts/wait";
+import { getAccessManageAcc } from "../../../lib/actions/user";
+
+test("del acc ok", async ({ browser }) => {
+  const { page, container } = await getAccessManageAcc(browser);
+
+  await waitTmr(page);
+
+  for (let i = 0; i < 2; i++) {
+    await clickByID(container, "btns_swapper_next_swap");
+    await waitTmr(page);
+  }
+
+  const form = await getByID(container, "delete_account__swap");
+
+  await clickByID(form, "delete_account__btn");
+
+  await waitTmr(page);
+
+  await clickByID(page, "pop__delete__btn");
+
+  await waitURL(page, "/notice");
+
+  await isToastOk(page, "account deleted");
+});
