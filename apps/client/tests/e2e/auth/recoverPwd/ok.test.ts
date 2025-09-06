8d4a2a62dca4ed88cdb10d78de61955e095777ad
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-09-06 12:42:40 +0300

    🪾 tests_2FA => 🛠️ passed both totp & backup code tests good case

--- apps/client/tests/e2e/auth/recoverPwd/ok.test.ts
+++ apps/client/tests/e2e/auth/recoverPwd/ok.test.ts
@@ -1,21 +1,9 @@
-import test, { expect } from "@playwright/test";
-import { genPwd } from "@/core/lib/pwd";
-import { REG_JWT } from "@/core/constants/regex";
+import test from "@playwright/test";
 import { preAuthRecoverPwd } from "./pre";
-import { clickByID, getByID } from "../../lib_tests/idx";
-import { waitURL } from "../../lib_tests/shortcuts/wait";
+import { recoverPwdOk } from "../../lib_tests/actions/auth";
 
 test("recover pwd ok", async ({ browser }) => {
-  const { form, page } = await preAuthRecoverPwd(browser);
+  const { page } = await preAuthRecoverPwd(browser);
 
-  const newPwd = genPwd();
-  await (await getByID(form, "password")).fill(newPwd);
-  await (await getByID(form, "confirm_password")).fill(newPwd);
-
-  await clickByID(form, "recover_pwd__form__submit");
-
-  await waitURL(page, "/");
-
-  const jwt = await page.evaluate(() => sessionStorage.getItem("access_token"));
-  expect(REG_JWT.test(jwt ?? ""));
+  await recoverPwdOk(page);
 });
--- apps/client/tests/e2e/lib_tests/actions/auth.ts
+++ apps/client/tests/e2e/lib_tests/actions/auth.ts
@@ -4,8 +4,10 @@ import { clickByID } from "../shortcuts/click";
 import { getByID, getByTxt } from "../shortcuts/get";
 import { waitTmr, waitURL } from "../shortcuts/wait";
 import { preAuthLogin } from "../../auth/login/pre";
-import { Browser } from "@playwright/test";
+import { Browser, expect, Page } from "@playwright/test";
 import { genRegisterPayload, PayloadRegisterT } from "../conf/payloads";
+import { genPwd } from "@/core/lib/pwd";
+import { REG_JWT } from "@/core/constants/regex";
 
 export const registerUserOk = async (
   browser: Browser,
@@ -72,4 +74,19 @@ export const loginUserOk = async (browser: Browser) => {
     payload,
     page,
   };
+};
+
+export const recoverPwdOk = async (page: Page) => {
+  const form = await getByID(page, "recover_pwd__form");
+
+  const newPwd = genPwd();
+  await (await getByID(form, "password")).fill(newPwd);
+  await (await getByID(form, "confirm_password")).fill(newPwd);
+
+  await clickByID(form, "recover_pwd__form__submit");
+
+  await waitURL(page, "/");
+
+  const jwt = await page.evaluate(() => sessionStorage.getItem("access_token"));
+  await expect(REG_JWT.test(jwt ?? "")).toBeTruthy();
 };
