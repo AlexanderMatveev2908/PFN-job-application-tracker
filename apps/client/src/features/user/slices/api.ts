468136fa80877eb3d9b2e52d4ddd052c355c29c2
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-09-06 13:44:28 +0300

    🪾 tests_2FA => 🛠️ _

--- apps/client/src/features/user/slices/api.ts
+++ apps/client/src/features/user/slices/api.ts
@@ -88,7 +88,7 @@ export const userSliceAPI = apiSlice.injectEndpoints({
       invalidatesTags: [TagAPI.USER],
     }),
 
-    goFormPreManageAcc2FA: builder.mutation<
+    getAccessManageAcc2FA: builder.mutation<
       ResApiT<GainAccessManageAccReturnT>,
       ParamsAPI2FA
     >({
