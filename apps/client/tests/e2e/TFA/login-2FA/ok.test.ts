e39ecc07586e59f10f65d443f9260e189d65dd85
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-09-06 09:34:55 +0300

    🪾 tests_2FA => 🛠️ tests login 2Fa passed both TOP & backup code

--- apps/client/tests/e2e/TFA/login-2FA/ok.test.ts
+++ apps/client/tests/e2e/TFA/login-2FA/ok.test.ts
@@ -1,37 +1,18 @@
 import test, { expect } from "@playwright/test";
-import {
-  clickByID,
-  getByID,
-  getByTxt,
-  getUser2FA,
-  isToastOk,
-  preTest,
-} from "../../lib/idx";
-import { waitURL } from "../../lib/shortcuts/wait";
+import { checkIsFocused, clickByID, getByID, isToastOk } from "../../lib/idx";
+import { waitTmr, waitURL } from "../../lib/shortcuts/wait";
 import { REG_JWT } from "@/core/constants/regex";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { authenticator, totp } from "otplib";
 import base32Decode from "base32-decode";
+import { preLogin2FA } from "./pre";
 
-test("login 2FA ok", async ({ browser }) => {
-  const { payload, totp_secret } = await getUser2FA(browser, {});
+test("login 2FA TOTP ok", async ({ browser }) => {
+  const { page, totp_secret } = await preLogin2FA(browser);
 
-  const page = await preTest(browser, "/auth/login");
+  const form = await getByID(page, "totp_code__form");
 
-  const formLogin = await getByID(page, "login__form");
-
-  await (await getByID(formLogin, "email")).fill(payload.email);
-  await (await getByID(formLogin, "password")).fill(payload.password);
-
-  await clickByID(formLogin, "login__form__submit");
-
-  await waitURL(page, "/auth/login-2FA");
-
-  const form2FA = await getByID(page, "2FA__form");
-
-  await getByTxt(form2FA, "totp code");
-
-  const firstSquare = await getByID(form2FA, "totp_code.0");
+  const firstSquare = await getByID(form, "totp_code.0");
   await firstSquare.click();
 
   await expect(firstSquare).toBeFocused();
@@ -50,7 +31,30 @@ test("login 2FA ok", async ({ browser }) => {
   await page.evaluate((c) => navigator.clipboard.writeText(c), code);
   await page.keyboard.press("Control+V");
 
-  await clickByID(form2FA, "totp_code__form__submit");
+  await clickByID(form, "totp_code__form__submit");
+
+  await waitURL(page, "/");
+  await isToastOk(page);
+
+  await expect(
+    await page.evaluate(() => sessionStorage.getItem("access_token"))
+  ).toMatch(REG_JWT);
+});
+
+test("login 2FA backup code ok", async ({ browser }) => {
+  const { page, backup_codes } = await preLogin2FA(browser);
+
+  await clickByID(page, "btns_swapper_next_swap");
+  await waitTmr(page);
+
+  const form = await getByID(page, "backup_code__form");
+  const codeInput = await getByID(form, "backup_code");
+
+  await checkIsFocused(codeInput);
+
+  await codeInput.fill(backup_codes[0]);
+
+  await clickByID(form, "backup_code__form__submit");
 
   await waitURL(page, "/");
   await isToastOk(page);
